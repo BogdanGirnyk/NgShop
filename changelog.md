78199e7c8b62b3c2ee81290a5aa6e7a0d799be17
Bogdan Girnyk <Bogdan.Girnyk@wolterskluwer.com>
2020-08-24 00:12:31 +0300

    Task1 implementation

---
+++ changelog.md
@@ -0,0 +1,11 @@
+## [1.0.0] - 2020-08-24
+### Implemented Task1
+### Added
+* components:
+  * FirstComponent
+  * ProductComponent
+  * ProductListComponent
+  * CartListComponent
+* services:
+  * ProductsService
+  * CartService
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FirstComponent } from './products/components/first-component/first.component';
+import { ProductComponent } from './products/components/product/product.component';
+import { ProductListComponent } from './products/components/product-list/product-list.component';
+import { ProductsService } from './products/services/products.service';
+import { CartService } from './products/services/cart.service';
+import { CartListComponent } from './products/components/cart-list/cart-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    ProductComponent,
+    ProductListComponent,
+    CartListComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ProductsService, CartService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
